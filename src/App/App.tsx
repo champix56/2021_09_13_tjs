fdcd10df5a96c0b85b5a1390be7f62207c4475c3
champix56 <desorbaix@free.fr>
2021-09-13 14:15:32 +0100

    1er cmp button + MEO

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-// import logo from './logo.svg';
+import Button from './components/Button/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      Demat breizh
+     <hr/>
+     <Button/>
     </div>
   );
 }
---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  './Button.css'
+
+const Button=()=>{
+    return (
+        <button className="button" >Bejamin</button>
+    );
+}
+
+export default Button;
