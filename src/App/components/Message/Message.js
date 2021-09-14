185d295917f17b736f74131543fe1f16cb818d1f
champix56 <desorbaix@free.fr>
2021-09-14 14:25:41 +0100

    gestion d'un message de liste

---
+++ src/App/components/Message/Message.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Message.module.scss';
+
+const Message = (props) => (
+  <div className={styles.Message} data-testid="Message">
+     <img src={props.message.user.img}/>
+     <div>
+       <div className={styles.messageHeader}>
+         {props.message.user.nick} à {props.message.dateTime}
+       </div>
+       <div className={styles.message}>
+         {props.message.text}
+       </div>
+     </div>
+  </div>
+);
+
+Message.propTypes = {};
+
+Message.defaultProps = {};
+
+export default Message;
