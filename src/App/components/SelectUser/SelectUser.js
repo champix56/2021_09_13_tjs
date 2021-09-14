e7e4ac23f2621d76db88acb94af17b36c37c2047
champix56 <desorbaix@free.fr>
2021-09-14 16:36:22 +0100

    portage de la valeur fourni par les props

--- src/App/components/SelectUser/SelectUser.js
+++ src/App/components/SelectUser/SelectUser.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './SelectUser.module.scss';
 
 const SelectUser = (props) => (
-  <select className={styles.SelectUser} data-testid="SelectUser" onChange={props.onChange}>
+  <select className={styles.SelectUser} data-testid="SelectUser" value={props.value} onChange={props.onChange}>
     {props.users.map((e, i) => <option value={e.id} key={'opt-usr-' + i}>{e.nick}</option>)}
   </select>
 );
