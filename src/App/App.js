f5f81b245106826a1ad956a65e3091b164a39af2
champix56 <desorbaix@free.fr>
2021-09-14 14:24:35 +0100

    passage vers un cmp de la constitution de page

--- src/App/App.js
+++ src/App/App.js
@@ -1,30 +1,11 @@
-import React from 'react';
-
-import './App.css';
-import Button from './components/Button/Button';
-
-class App extends React.Component {
-  // counter = 0
-  constructor(props)
-  {
-    super(props);
-    this.state={counter:0,name:'Alexandre'};
-  }
+import React, { Component } from 'react';
+import './App.css'
+import Tchat from './pages/Tchat/Tchat';
+class App extends Component {
   render() {
     return (
       <div className="App">
-        Valeur du counter : {this.state.counter}
-        <hr />
-        <Button onClickEvent={ () => {
-          // this.state.counter--;
-          this.setState({counter:this.state.counter-1})
-          console.log(this.state.counter);
-        }}>soustraction</Button>
-        <Button onClickEvent={() => {
-          // this.state.counter++;
-          this.setState({counter:this.state.counter+1})
-          console.log(this.state.counter);
-        }} bgColor="chartreuse">addition</Button>
+        <Tchat/>
       </div>
     );
   }
